f5231a16f8b1e9c2db27599a5f46cdbd0a3a108c
Abdelilah Ouchani <abdelilahouchani3245@gmail.com>
2017-12-26 08:32:39 +0000

    improving some lines

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -7,55 +7,65 @@
 // })(jQuery); // end of jQuery name space
 // //////////
 $(document).ready(function () {
-  $('.parallax').parallax();
-  $(".dropdown-button").dropdown();
-  $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+    $(".dropdown-button").dropdown();
+    $(".button-collapse").sideNav();
 
 });
 /////Angular////
 
-angular.module("felinaApp", []).controller("Controller", function ($scope,$http) {
-  $scope.word = '';
-  $scope.isCaseSensitive = false;
+angular.module("felinaApp", []).controller("Controller", function ($scope, $http) {
+        $scope.word = '';
+        $scope.isCaseSensitive = false;
 
-  $scope.getPhrases = function (value) {
-    $http({
-      method: 'GET',
-      url: 'http://localhost:8080/getFromDictionary/'+$scope.word+'&&'+ $scope.isCaseSensitive
-    }).then(function successCallback(response) {
-      $scope.result = response.data;
-      if($scope.result.length==0) Materialize.toast('Item non existent',1000);
-      $scope.gotResult = true;
-    });
-  };
+        $scope.getPhrases = function (value) {
+            $http({
+                method: 'GET',
+                url: 'getFromDictionary/' + $scope.word + '&&' + $scope.isCaseSensitive
+            }).then(function successCallback(response) {
+                $scope.result = response.data;
+                if ($scope.result.length == 0) Materialize.toast('Item non existent', 1000);
+                $scope.gotResult = true;
+            });
+        };
+        $scope.add = function (value) {
+            if ($scope.word.length != 0) {
+                $http({
+                    method: 'GET',
+                    url: '/addToDictionary/' + $scope.word
+                }).then(function successCallback(response) {
+                    if (response.data) Materialize.toast('Phrase added!', 800, 'rounded');
+                    else Materialize.toast('Phrase already exists on \'Database\'!', 800, 'rounded')
+                }, function errorCallback(response) {
+                    Materialize.toast('Uknown Error !', 800);
+                });
+            }
+            else Materialize.toast('Please Enter a Phrase!', 800);
 
-  $scope.add = function(value){
-    $http({
-      method: 'GET',
-      url: 'http://localhost:8080/addToDictionary/'+$scope.word
-    }).then(function successCallback(response) {
-      if(response.data) Materialize.toast('Phrase added!', 1000,'rounded') ;
-      else Materialize.toast('Phrase already exists on \'Database\'!', 1000,'rounded')
-    }, function errorCallback(response) {
-      Materialize.toast('Please Enter a Phrase!', 1000) ;
-    });
-  }
-   
-  $scope.remove = function(value){
-    $http({
-      method: 'GET',
-      url: 'http://localhost:8080/removeFromDictionary/'+$scope.word
-    }).then(function successCallback(response) {
-      if(response.data) Materialize.toast('Phrase Deleted!', 1000) ;
-      else Materialize.toast('Element Not deleted, ', 1000)
-    }, function errorCallback(response) {
-      Materialize.toast('Enter A WORD !..', 1000) 
-    });
-  }
 
-    $scope.soon = function(value){
-        Materialize.toast('Soon :) ..', 1500, 'rounded');
+        }
+
+        $scope.remove = function (value) {
+            if ($scope.word.length != 0) {
+                $http({
+                    method: 'GET',
+                    url: 'removeFromDictionary/' + $scope.word
+                }).then(function successCallback(response) {
+                    if (response.data) Materialize.toast('Phrase Deleted!', 800);
+                    else Materialize.toast('Element Not deleted, ', 1000)
+                }, function errorCallback(response) {
+                    Materialize.toast('Uknown Error !', 800);
+                });
+            }
+            else {
+                Materialize.toast('Please Enter a Phrase!', 800);
+            }
+        }
+
+        $scope.soon = function (value) {
+            Materialize.toast('Soon :) ..', 1500, 'rounded');
+        }
+
+
     }
-
-
-});
+);
